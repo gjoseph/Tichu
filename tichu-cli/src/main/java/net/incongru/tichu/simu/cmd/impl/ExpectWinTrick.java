e652f5c6b1232cc3701d139ffd03eb644bf14863
Gregory Joseph <gjoseph@atlassian.com>
2020-02-19 02:36:09 +1100

    Simu: fix expect-win-trick since Trick now stores instance of Played rather than Play

--- tichu-cli/src/main/java/net/incongru/tichu/simu/cmd/impl/ExpectWinTrick.java
+++ tichu-cli/src/main/java/net/incongru/tichu/simu/cmd/impl/ExpectWinTrick.java
@@ -1,6 +1,7 @@
 package net.incongru.tichu.simu.cmd.impl;
 
 import net.incongru.tichu.action.Action;
+import net.incongru.tichu.model.Player;
 import net.incongru.tichu.model.Trick;
 import net.incongru.tichu.simu.SimulatedGameContext;
 import net.incongru.tichu.simu.Simulation;
@@ -19,14 +20,14 @@ class ExpectWinTrick extends ExpectPlayResult {
         super.doExec(ctx, result);
 
         final Trick trick = ctx.game().currentRound().currentTrick();
-        final boolean match = trick.isDone() && trick.currentPlayer().name().equals(expectedPlayerName);
+
+        final Player lastPlayer = trick.previousNonPass().player();
+        final boolean match = trick.isDone() && lastPlayer.name().equals(expectedPlayerName);
         if (match) {
-            ctx.log("Trick is done, as expected. Current player is indeed %s.", expectedPlayerName);
+            ctx.log("Trick is done and won by %s, as expected.", expectedPlayerName);
         } else {
-            ctx.log("Trick is %, current player is %s", trick.isDone()?"done":"not done", trick.currentPlayer());
-            // TODO... oh no this isn't going to work. currentPlayer is not the winner, it's going to be the last pass-er
-            //  (in some cases at least)
-            throw new Simulation.PostActionCommandException("Trick was expected to be 'done' at this point, with %s as the winner");
+            ctx.log("Trick is %, last player is %s", trick.isDone() ? "done" : "not done", lastPlayer);
+            throw new Simulation.PostActionCommandException("Trick was expected to be 'done' at this point, with %s as the winner", expectedPlayerName);
         }
 
     }
