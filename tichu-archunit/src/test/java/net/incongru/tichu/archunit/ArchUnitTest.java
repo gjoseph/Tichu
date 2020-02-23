fda87837bbd817dd761be6a757a2e821fb432b49
Gregory Joseph <gjoseph@atlassian.com>
2020-02-23 22:00:46 +1100

    Simplify first ArchUnit test, combining both assertions in one

--- tichu-archunit/src/test/java/net/incongru/tichu/archunit/ArchUnitTest.java
+++ tichu-archunit/src/test/java/net/incongru/tichu/archunit/ArchUnitTest.java
@@ -4,7 +4,6 @@ import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.lang.ArchRule;
 
-import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.constructors;
 
 @AnalyzeClasses(packages = "net.incongru.tichu")
@@ -12,16 +11,11 @@ public class ArchUnitTest {
     // TODO // Other interesting rules in com.tngtech.archunit.library.GeneralCodingRules
 
     @ArchTest
-    public static final ArchRule classes_in_impl_packages_should_be_package_private =
-            classes()
-                    .that().resideInAPackage("..impl")
-                    .and().haveSimpleNameNotEndingWith("FactoryImpl")
-                    .should().bePackagePrivate();
-
-    @ArchTest
-    public static final ArchRule ctors_in_impl_packages_should_be_package_private =
+    static final ArchRule classes_and_ctors_in_impl_packages_should_be_package_private =
             constructors()
                     .that().areDeclaredInClassesThat().resideInAPackage("..impl")
                     .and().areDeclaredInClassesThat().haveSimpleNameNotEndingWith("FactoryImpl")
-                    .should().bePackagePrivate();
+                    .should().bePackagePrivate()
+                    .andShould().beDeclaredInClassesThat().arePackagePrivate();
+
 }
