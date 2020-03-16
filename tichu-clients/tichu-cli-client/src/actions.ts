70fc110ab86a840ccf0c4460efd10931c650d0bc
Gregory Joseph <gjoseph@atlassian.com>
2020-03-16 22:12:28 +1100

    Basic prompt with inquirer

---
+++ tichu-clients/tichu-cli-client/src/actions.ts
@@ -0,0 +1,31 @@
+// init
+class InitParam {
+    constructor() {
+    }
+}
+
+// join
+class JoinParam {
+    constructor(readonly  playerName: string, team: number) {
+    }
+}
+
+// newTrick?
+
+// isReady
+class PlayerIsReadyParam {
+    constructor(readonly  playerName: string) {
+    }
+}
+
+// play
+class PlayerPlaysParam {
+    constructor(readonly  playerName: string, readonly cards: string[]) {
+    }
+}
+
+// pass = play[]
+
+export enum Actions {
+    init = "init", join = "join", ready = "ready", play = "play", pass = "pass"
+}
--- tichu-clients/tichu-cli-client/src/index.ts
+++ tichu-clients/tichu-cli-client/src/index.ts
@@ -1,2 +1,32 @@
-// happy coding 👻
-console.log("hello world");
+import inquirer from "inquirer";
+import {Actions} from "./actions";
+
+inquirer.prompt([
+    {
+        type: 'list',
+        name: 'action',
+        message: 'What do you want to do?',
+        choices: [Actions.init, Actions.join, Actions.ready, Actions.play, Actions.pass]
+    },
+    {
+        when: (answers: any) => {
+            return answers.action === Actions.play
+        },
+        type: "checkbox",
+        name: "cards",
+        message: "Pick cards to play",
+        choices: [
+            {name: "first", value: 1},
+            {name: "second", value: 2},
+        ]
+    }
+]).then((answers: any) => {
+    console.log("Answers:", answers.constructor.name, answers)
+}).catch((error: any) => {
+    console.log("Error", error.constructor.name);
+    if (error.isTtyError) {
+        // Prompt couldn't be rendered in the current environment
+    } else {
+        // Something else when wrong
+    }
+});
