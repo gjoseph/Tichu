659530f999678efed804fb2edce3c24a0764ce27
Gregory Joseph <greg.jos@gmail.com>
2020-07-08 19:02:58 +1000

    Remove sample logo.svg and App.css, adapted title of sample test

--- tichu-clients/packages/tichu-web/src/App.test.tsx
+++ tichu-clients/packages/tichu-web/src/App.test.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { render } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("renders at least a card", () => {
   const { getByText } = render(<App />);
   expect(getByText(/Phoenix/)).toBeInTheDocument();
 });
--- tichu-clients/packages/tichu-web/src/App.tsx
+++ tichu-clients/packages/tichu-web/src/App.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import "./App.css";
 import { Table } from "./components/Table";
 
 function App() {
