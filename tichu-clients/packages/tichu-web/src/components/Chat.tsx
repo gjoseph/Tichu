c6613d3139ad9b99b4b6c35bc68aaf8735bca990
Gregory Joseph <greg.jos@gmail.com>
2020-07-17 23:35:11 +1000

    Enable sending chat messages

--- tichu-clients/packages/tichu-web/src/components/Chat.tsx
+++ tichu-clients/packages/tichu-web/src/components/Chat.tsx
@@ -1,9 +1,26 @@
-import { IncomingChatMessage } from "tichu-client-ts-lib";
-import React, { FC } from "react";
+import { IncomingChatMessage, OnResponse } from "tichu-client-ts-lib";
+import React, { FC, FormEvent, useState } from "react";
+import { LinearProgress } from "@material-ui/core";
+// import styles from "./Chat.module.css";
 
 // The component's model will likely be different from the actual websocket messages:
 export type ChatMessage = IncomingChatMessage;
-export const Chat: FC<{ chatMessages: ChatMessage[] }> = ({ chatMessages }) => {
+export const Chat: FC<{
+  chatMessages: ChatMessage[];
+  sendChatMessage: (text: string, onResponse: OnResponse) => void;
+}> = ({ chatMessages, sendChatMessage }) => {
+  const [sending, setSending] = useState(false);
+  const [messageText, setMessageText] = useState("");
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    // spinner until new message comes in
+    setSending(true);
+    sendChatMessage(messageText, stopSendSpinner);
+    setMessageText("");
+  };
+  const stopSendSpinner = () => {
+    setSending(false);
+  };
   return (
     <div>
       <h3>Chat</h3>
@@ -14,6 +31,20 @@ export const Chat: FC<{ chatMessages: ChatMessage[] }> = ({ chatMessages }) => {
           </li>
         ))}
       </ul>
+      {sending ? (
+        <LinearProgress />
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <label>
+            Chat:
+            <textarea
+              value={messageText}
+              onChange={(e) => setMessageText(e.target.value)}
+            />
+          </label>
+          <input type="submit" value="Send" />
+        </form>
+      )}
     </div>
   );
 };
