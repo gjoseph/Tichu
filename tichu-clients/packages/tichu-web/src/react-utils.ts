f4164383331487103c0f5854baaadea716bc6118
Gregory Joseph <greg.jos@gmail.com>
2020-07-19 18:08:39 +1000

    Add a ConnectivityIndicator component

--- tichu-clients/packages/tichu-web/src/react-utils.ts
+++ tichu-clients/packages/tichu-web/src/react-utils.ts
@@ -48,3 +48,22 @@ export function useStoredState<T>(
   };
   return [value, setValue, removeValue];
 }
+
+/**
+ * Copied and adapted from https://medium.com/the-non-traditional-developer/checking-the-network-connection-with-a-react-hook-ec3d8e4de4ec
+ */
+export function useNetworkAvailability() {
+  const [isOnline, setNetwork] = React.useState(window.navigator.onLine);
+  const updateNetwork = () => {
+    setNetwork(window.navigator.onLine);
+  };
+  React.useEffect(() => {
+    window.addEventListener("offline", updateNetwork);
+    window.addEventListener("online", updateNetwork);
+    return () => {
+      window.removeEventListener("offline", updateNetwork);
+      window.removeEventListener("online", updateNetwork);
+    };
+  });
+  return isOnline;
+}
