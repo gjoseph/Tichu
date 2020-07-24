44414f55ebdc0e20f25b57f422b760a14bba7a86
Gregory Joseph <greg.jos@gmail.com>
2020-07-25 01:03:02 +1000

    web: Extracted OtherPlayer component (wip), introduced PlayerAvatar component
    * changed User.name to User.displayName, added User.avatarUrl

---
+++ tichu-clients/packages/tichu-web/src/model/User.test.ts
@@ -0,0 +1,15 @@
+import { User } from "./User";
+
+test("initials for simple 2 word name", () => {
+  expect(new User("x", "Alex Charlie").initials()).toBe("AC");
+});
+test("initials for 1 word name should be the single letter", () => {
+  expect(new User("x", "Charlie").initials()).toBe("C");
+});
+test("initials for longer name should be limited to 2", () => {
+  expect(new User("x", "Alex Charlie Jules Quinn").initials()).toBe("AC");
+});
+test("initials for falsy name should be null", () => {
+  expect(new User("x", "").initials()).toBeNull();
+  expect(new User("x", "  ").initials()).toBeNull();
+});
--- tichu-clients/packages/tichu-web/src/model/User.ts
+++ tichu-clients/packages/tichu-web/src/model/User.ts
@@ -1,5 +1,23 @@
 import { UserId } from "tichu-client-ts-lib";
 
+// Assuming we'll be using something like Auth0, it'll provide an avatar url
 export class User {
-  constructor(readonly id: UserId, readonly name: string) {}
+  constructor(
+    readonly id: UserId,
+    readonly displayName?: string,
+    readonly avatarUrl?: string
+  ) {}
+
+  initials(): string | null {
+    const trimmed = this.displayName?.trim();
+    if (!trimmed) {
+      return null;
+    }
+    return trimmed
+      .split(/\s+/)
+      .slice(0, 2)
+      .map((s) => s[0])
+      .join("")
+      .toUpperCase();
+  }
 }
