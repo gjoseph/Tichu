864ca0af522981ee2a458d88a4a387a41537ada3
Gregory Joseph <greg.jos@gmail.com>
2020-07-26 16:14:06 +1000

    Simplify avatar color hash

---
+++ tichu-clients/packages/tichu-web/src/util.ts
@@ -0,0 +1,18 @@
+/**
+ * Copied and adapted from https://medium.com/@pppped/compute-an-arbitrary-color-for-user-avatar-starting-from-his-username-with-javascript-cd0675943b66,
+ * which seems much simpler and lighter than depending on color-hash.
+ */
+export const stringToHslColor = (
+  s: string,
+  // TODO i've had nicer results with 80, 50 and hash<<9, but not sure why
+  saturation: number = 80,
+  lightness: number = 80
+) => {
+  let hash = 0;
+  for (let i = 0; i < s.length; i++) {
+    hash = s.charCodeAt(i) + ((hash << 5) - hash);
+  }
+
+  const h = hash % 360;
+  return "hsl(" + h + ", " + saturation + "%, " + lightness + "%)";
+};
