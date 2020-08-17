8225731e97372dfaa2284b76819871016367b95d
Gregory Joseph <greg.jos@gmail.com>
2020-08-17 19:01:43 +1000

    server: Send a GameStatusMessage

--- tichu-websocket-endpoint/src/main/java/net/incongru/tichu/websocket/OutgoingMessage.java
+++ tichu-websocket-endpoint/src/main/java/net/incongru/tichu/websocket/OutgoingMessage.java
@@ -14,6 +14,8 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo;
         // but we still need to also specify @JsonDeserialize(as) on the abstract types for deserialisation
         @JsonSubTypes.Type(value = ImmutableOutgoingChatMessage.class, name = "chat"),
         @JsonSubTypes.Type(value = ImmutableGameActionResultMessage.class, name = "game"),
+        // perhaps this should actually be part of ImmutableGameActionResultMessage
+        @JsonSubTypes.Type(value = ImmutableGameStatusMessage.class, name = "status"),
         @JsonSubTypes.Type(value = ImmutablePlayerHandMessage.class, name = "hand"),
         @JsonSubTypes.Type(value = ImmutableErrorMessage.class, name = "error"),
         @JsonSubTypes.Type(value = ImmutableRoomActivityMessage.class, name = "activity")
