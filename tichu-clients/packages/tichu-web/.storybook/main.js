e1fae1c0a33de7f4dd281179852b1b198c4003da
Gregory Joseph <greg.jos@gmail.com>
2021-10-09 19:48:56 +1100

    Fix storybook config to use webpack5 after upgrade to sb6

--- tichu-clients/packages/tichu-web/.storybook/main.js
+++ tichu-clients/packages/tichu-web/.storybook/main.js
@@ -1,5 +1,8 @@
 module.exports = {
-  stories: ["../src/**/*.stories.(ts|tsx|js|jsx|mdx)"],
+  core: {
+    builder: "webpack5",
+  },
+  stories: ["../src/**/*.stories.@(ts|tsx|js|jsx|mdx)"],
   addons: [
     "@storybook/preset-create-react-app",
     "@storybook/addon-actions",
