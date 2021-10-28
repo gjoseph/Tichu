ba1792c0258d581b452e67163dfd3f1ddd223fcc
Gregory Joseph <greg.jos@gmail.com>
2021-10-28 20:42:10 +1100

    Hopefully we won't need regen-lock-files.sh anymore, but here it is, de-lerna-fied

--- tichu-clients/regen-lock-files.sh
+++ tichu-clients/regen-lock-files.sh
@@ -3,21 +3,18 @@ set -o errtrace
 set -o functrace
 set -o nounset
 
-rm -f package-lock.json packages/*/package-lock.json
-
-# Remove node_modules
-npx lerna clean -y
+echo Deleting package-lock.json and node_modules ...
+rm -rf package-lock.json \
+       node_modules \
+       packages/*/node_modules \
+       packages/*/package-lock.json
 
 # Install root -- same command as Renovate uses.
-npm install --ignore-scripts --no-audit --package-lock-only
-
-# Lerna bootstrap -- same command as Renovate uses. Not sure if/what the -- flags do.
-npx lerna bootstrap --no-ci --ignore-scripts # -- --ignore-scripts --no-audit --package-lock-only
+echo Running npm install ...
+npm install --ignore-scripts --no-audit --package-lock-only # --force may be needed to work around the cra5/sb6 shenanigans
 
 # Reformat
+echo Making it pretty ...
 npm run prettier-fix-all
 
 git diff --exit-code
-
-# For some reason this produces different results:
-# npm i && npx lerna exec --concurrency 1 -- npm i
