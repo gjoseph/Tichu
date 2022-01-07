c8a0c850f2a927f25c6e3788224ce1f97153a866
Gregory Joseph <greg.jos@gmail.com>
2022-01-08 00:47:38 +1100

    Also ignore ws in storybook's webpack config

--- tichu-clients/packages/tichu-web/.storybook/main.js
+++ tichu-clients/packages/tichu-web/.storybook/main.js
@@ -1,6 +1,12 @@
+const webpack = require("webpack");
 module.exports = {
   core: {
     builder: "webpack5",
+  },
+  webpackFinal: async (config) => {
+    // see craco.config.js
+    config.plugins.push(new webpack.IgnorePlugin({ resourceRegExp: /^ws$/ }));
+    return config;
   },
   stories: ["../src/**/*.stories.@(ts|tsx|js|jsx|mdx)"],
   addons: [
