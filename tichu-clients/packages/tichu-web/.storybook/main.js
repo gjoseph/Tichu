a005f7f8560270f1e9baf8baa53d75af3a1c0d94
Gregory Joseph <greg.jos@gmail.com>
2022-07-21 20:00:16 +1000

    storybook: replace knobs with args&controls
    * setup
    * first story - CardSet

--- tichu-clients/packages/tichu-web/.storybook/main.js
+++ tichu-clients/packages/tichu-web/.storybook/main.js
@@ -11,14 +11,12 @@ module.exports = {
   stories: ["../src/**/*.stories.@(ts|tsx|js|jsx|mdx)"],
   addons: [
     "@storybook/preset-create-react-app",
-    "@storybook/addon-actions",
     "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-storysource",
     "@storybook/addon-knobs",
-    {
-      name: "@storybook/addon-docs",
-      options: {
-        configureJSX: true,
-      },
-    },
   ],
+  typescript: {
+    check: true, // type-check stories during Storybook build
+  },
 };
--- tichu-clients/packages/tichu-web/src/stories/CardSet.stories.tsx
+++ tichu-clients/packages/tichu-web/src/stories/CardSet.stories.tsx
@@ -2,45 +2,74 @@ import { actions } from "@storybook/addon-actions";
 import React from "react";
 import { AllCards } from "tichu-client-ts-lib";
 import { CardBacks, CardSet } from "../components/CardSet";
-import { countKnob, sizeKnob, styleKnob } from "./knobs";
+import { Args, Meta, Story } from "@storybook/react";
+import { countControl } from "./controls";
+import { makeStory } from "./stories";
 
 export default {
   title: "Card Set",
-  component: CardSet,
-};
+  component: CardSet, // and CardBacks
+  argTypes: {
+    cardCount: countControl(56),
+    cards: {
+      control: false,
+    },
+    onCardClick: {
+      control: false,
+    },
+  },
+  args: {
+    cardCount: 3,
+    cardSize: "small",
+    layout: "flat",
+  },
+} as Meta;
+
 const events = actions("onCardClick");
 
-export const Card_Set = () => {
+export const Card_Set: Story = makeStory((args: Args) => {
   const cards = [...AllCards]
     .sort(() => Math.random() - 0.5)
-    .slice(0, countKnob(14))
+    .slice(0, args.cardCount)
     .map((c) => ({ ...c, selected: false }));
   return (
     <CardSet
       cards={cards}
-      cardSize={sizeKnob()}
-      layout={styleKnob()}
+      cardSize={args.cardSize}
+      layout={args.layout} // not sure why ...args doesn't work.. something to do with types
       {...events}
     />
   );
-};
+});
 
-export const Stack_of_56 = () => (
-  <CardSet
-    cards={AllCards.map((c) => ({ ...c, selected: false }))}
-    cardSize={sizeKnob()}
-    layout="stacked"
-    {...events}
-  />
+export const Stack_of_56 = makeStory(
+  (args: Args) => {
+    return (
+      <CardSet
+        cards={AllCards.map((c) => ({ ...c, selected: false }))}
+        cardSize={args.cardSize}
+        layout={args.layout}
+        {...events}
+      />
+    );
+  },
+  {
+    layout: "stacked",
+  },
+  {
+    cardCount: {
+      control: false,
+    },
+  }
 );
 
-export const Card_Backs = () => {
+export const Card_Backs: Story = makeStory((args: Args) => {
   return (
     <CardBacks
-      count={countKnob(56)}
-      cardSize={sizeKnob()}
-      layout={styleKnob()}
+      count={args.cardCount}
+      cardSize={args.cardSize}
+      layout={args.layout}
       {...events}
     />
   );
-};
+});
