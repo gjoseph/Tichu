95ab6f5035ac8f6c43a18b8382b381c231e6ca13
Gregory Joseph <greg.jos@gmail.com>
2022-07-21 20:27:11 +1000

    storybook: welcome story

--- tichu-clients/packages/tichu-web/src/stories/0-Welcome.stories.tsx
+++ tichu-clients/packages/tichu-web/src/stories/0-Welcome.stories.tsx
@@ -1,13 +1,21 @@
 import React from "react";
-import { linkTo } from "@storybook/addon-links";
-import { Welcome } from "@storybook/react/demo";
 
 export default {
   title: "Welcome",
-  component: Welcome,
 };
 
-export const ToStorybook = () => <Welcome showApp={linkTo("Button")} />;
+const text = `These are bunch of Storybook stories to help me build Tichu components, learn React, re-learn
+React and the entire JS stack every 3 months, and mostly just get frustrated at this abominable
+stack. Which is weird, cause it's been around for so long, produces so many amazing apps, and 
+full of talented engineers, you wonder how it's not been fixed yet.`;
+
+// Keeping the story for the sake of having an entry point, but not using sb's demo
+export const ToStorybook = () => (
+  <div>
+    <h1>Welcome</h1>
+    <pre>{text}</pre>
+  </div>
+);
 
 ToStorybook.story = {
   name: "to Storybook",
