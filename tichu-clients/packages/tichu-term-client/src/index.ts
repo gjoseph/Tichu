3eaa4c74def374bee4679e57bcd481757a7e0e99
Gregory Joseph <greg.jos@gmail.com>
2024-11-19 23:26:48 +1100

    Remove inquirer usage from startup
    * clunky, and thought this might make migration to v12 easier

--- tichu-clients/packages/tichu-term-client/src/index.ts
+++ tichu-clients/packages/tichu-term-client/src/index.ts
@@ -1,10 +1,13 @@
-import inquirer from "inquirer";
 import { program } from "commander";
-import { GameOpts, setupQuestions } from "./startup";
 import { newTerminalHandler } from "./term-handler";
 import { WSTichuClient } from "tichu-client-ts-lib";
 
 const url = "ws://localhost:8080/room/some-room-id";
+
+interface GameOpts {
+  room: string;
+  user: string;
+}
 
 program
   .storeOptionsAsProperties(false)
@@ -17,15 +20,6 @@ program
 program
   .parseAsync()
   .then((program) => program.opts() as GameOpts)
-  .then((opts: GameOpts) => {
-    return inquirer.prompt(setupQuestions(opts)).then((answers: any) => {
-      // Combine CLI opts and answers
-      return {
-        room: answers.roomId ?? opts.room,
-        user: answers.userId ?? opts.user,
-      } as GameOpts;
-    });
-  })
   .then((opts: GameOpts) => {
     console.log("Connecting to room ...", opts);
     return new WSTichuClient(opts.user, newTerminalHandler).connect(url);
