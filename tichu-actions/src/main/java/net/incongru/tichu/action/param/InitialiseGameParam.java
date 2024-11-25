e19036470f5f76ee22f617238f34fb53b085b04d
Gregory Joseph <greg.jos@gmail.com>
2024-11-25 20:44:10 +1100

    Replace Immutables by a record class for InitialiseGameParam

--- tichu-actions/src/main/java/net/incongru/tichu/action/param/InitialiseGameParam.java
+++ tichu-actions/src/main/java/net/incongru/tichu/action/param/InitialiseGameParam.java
@@ -2,13 +2,10 @@ package net.incongru.tichu.action.param;
 
 import net.incongru.tichu.action.ActionParam;
 import net.incongru.tichu.model.UserId;
-import org.immutables.value.Value;
 
-@Value.Immutable
-public interface InitialiseGameParam extends ActionParam {
+public record InitialiseGameParam() implements ActionParam {
 
-    static WithActor<InitialiseGameParam> withActor(UserId player) {
-        return new WithActor<>(player,
-                ImmutableInitialiseGameParam.builder().build());
+    public static WithActor<InitialiseGameParam> withActor(UserId player) {
+        return new WithActor<>(player, new InitialiseGameParam());
     }
 }
