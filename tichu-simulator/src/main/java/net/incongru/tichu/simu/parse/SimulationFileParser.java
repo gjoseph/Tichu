a4d0fddc35a5e9cdf81a844b08cd6ac0629e0d94
Gregory Joseph <greg.jos@gmail.com>
2024-11-25 21:27:16 +1100

    Replace Immutables by a record class for Simulation

--- tichu-simulator/src/main/java/net/incongru/tichu/simu/parse/SimulationFileParser.java
+++ tichu-simulator/src/main/java/net/incongru/tichu/simu/parse/SimulationFileParser.java
@@ -1,13 +1,12 @@
 package net.incongru.tichu.simu.parse;
 
 import net.incongru.tichu.action.ActionParam;
-import net.incongru.tichu.simu.ImmutableActionAndCommands;
-import net.incongru.tichu.simu.ImmutableSimulation;
 import net.incongru.tichu.simu.Simulation;
 import net.incongru.tichu.simu.cmd.impl.PostActionCommandFactoryImpl;
 
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 
 public class SimulationFileParser {
@@ -23,30 +22,25 @@ public class SimulationFileParser {
     }
 
     public Simulation parse(Path p) throws IOException {
-        final ImmutableSimulation.Builder builder = ImmutableSimulation.builder();
+        final List<Simulation.ActionAndCommands> actionAndCommands = new ArrayList<>();
         final List<String> lines = SimulationFileLoader.from(p);
         int i = 0;
         while (i < lines.size()) {
             final TokenisedLine tokens = new TokenisedLine(lines.get(i));
             final ActionParam.WithActor param = actionLineParsers.parse(tokens);
-            final ImmutableActionAndCommands.Builder actionAndCommandsBuilder = ImmutableActionAndCommands.builder().actionParam(param);
+            final Simulation.ActionAndCommands.Builder actionAndCommandsBuilder = Simulation.ActionAndCommands.builder().actionParam(param);
             while (nextLineIsExpectation(lines, i)) {
                 i++;
                 final TokenisedLine cmdTokens = new TokenisedLine(lines.get(i));
                 cmdTokens.pop(0); // strip leading dash -- TODO this whole loop could be improved, but needs test before refactor
                 final Simulation.PostActionCommand cmd = pacLineParsers.parse(cmdTokens);
-                actionAndCommandsBuilder.addCommands(cmd);
+                actionAndCommandsBuilder.addCommand(cmd);
             }
 
-            final ImmutableActionAndCommands actionAndCommands = actionAndCommandsBuilder.build();
-            if (actionAndCommands.commands().isEmpty()) {
-                builder.addActionAndCommands(actionAndCommands.withCommands(defaultPostActionCommand()));
-            } else {
-                builder.addActionAndCommands(actionAndCommands);
-            }
+            actionAndCommands.add(actionAndCommandsBuilder.buildWithDefaultCommand(defaultPostActionCommand()));
             i++;
         }
-        return builder.build();
+        return new Simulation(actionAndCommands);
     }
 
 
