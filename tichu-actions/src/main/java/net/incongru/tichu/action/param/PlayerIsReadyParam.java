ef465ea7ba1e5960f1580fea41b9f99fc729cfa0
Gregory Joseph <greg.jos@gmail.com>
2024-11-25 20:50:26 +1100

    Replace Immutables by a record class for PlayerIsReadyParam

--- tichu-actions/src/main/java/net/incongru/tichu/action/param/PlayerIsReadyParam.java
+++ tichu-actions/src/main/java/net/incongru/tichu/action/param/PlayerIsReadyParam.java
@@ -2,12 +2,10 @@ package net.incongru.tichu.action.param;
 
 import net.incongru.tichu.action.ActionParam;
 import net.incongru.tichu.model.UserId;
-import org.immutables.value.Value;
 
-@Value.Immutable
-public interface PlayerIsReadyParam extends ActionParam {
+public record PlayerIsReadyParam() implements ActionParam {
 
-    static WithActor<PlayerIsReadyParam> withActor(UserId player) {
-        return new WithActor<>(player, ImmutablePlayerIsReadyParam.builder().build());
+    public static WithActor<PlayerIsReadyParam> withActor(UserId player) {
+        return new WithActor<>(player, new PlayerIsReadyParam());
     }
 }
