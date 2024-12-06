c9b3914552819592a851ec7d6dac75543014f81d
Gregory Joseph <gjoseph@atlassian.com>
2024-12-06 12:15:00 +1100

    model: UserId is now a record class

--- tichu-model/src/main/java/net/incongru/tichu/model/UserId.java
+++ tichu-model/src/main/java/net/incongru/tichu/model/UserId.java
@@ -1,41 +1,11 @@
 package net.incongru.tichu.model;
 
-import java.util.Objects;
-import javax.annotation.Nonnull;
-
 /**
  * A UserId represents a user's... id. It might well be an external system's ID provided by e.g. Auth0 or Okta,
  * or in the case of a simulation a readable fake-human name.
- *
- * TODO @see UserDetailsProvider
  */
-public class UserId {
-
+public record UserId(String id) {
     public static UserId of(String id) {
         return new UserId(id);
-    }
-
-    private final String id;
-
-    private UserId(@Nonnull String id) {
-        this.id = id;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        UserId userId = (UserId) o;
-        return id.equals(userId.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
-
-    @Override
-    public String toString() {
-        return "UserId{" + "id='" + id + '\'' + '}';
     }
 }
