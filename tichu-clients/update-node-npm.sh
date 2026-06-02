db21bcad340c6e5a86910097c19f6b186966e63d
Gregory Joseph <greg.jos@gmail.com>
2026-06-02 22:45:34 +1000

    Use mise instead of asdf
    * fd can't currently be installed via mise or aqua since it's lacking a darwin/amd64 package - see https://github.com/sharkdp/fd/pull/1946

--- tichu-clients/update-node-npm.sh
+++ tichu-clients/update-node-npm.sh
@@ -1,26 +1,20 @@
-set -o errexit
-set -o errtrace
-set -o functrace
-set -o nounset
+#!/usr/bin/env zsh
+set -euo pipefail
 
 # ensure pwd = dir of $0
 dir0="$(dirname $0)"
 dir0_resolved="$(readlink -f ${dir0})"
 cd "${dir0_resolved}"
 
-#echo Updating nodejs asdf plugin...
-#not doing this since that would overwrite my fix for npm_location(can't install -g modules
-#asdf plugin update nodejs
-
 if [ "$#" -eq 2 ]; then
   NODE_VERSION=$1
   NPM_VERSION=$2
 else
   echo Working out latest versions...
-  CURRENT=$(asdf current --no-header nodejs | awk '{print $2}')
-  CURRENT_MAJOR=$(echo "${CURRENT}" | grep -Eo '^(\d)+\.') # includes dot just for explicitness, asdf is happy with it
-  LATEST_ON_CURRENT_MAJOR=$(asdf latest nodejs "${CURRENT_MAJOR}")
-  LATEST=$(asdf latest nodejs)
+  CURRENT=$(mise tool node --json | jq -r '.active_versions[0]')
+  CURRENT_MAJOR=$(echo "${CURRENT}" | grep -Eo '^(\d)+')
+  LATEST_ON_CURRENT_MAJOR=$(mise latest node@"${CURRENT_MAJOR}")
+  LATEST=$(mise latest node)
   echo Current nodejs: $CURRENT
   echo Latest nodejs on same major version as current: $LATEST_ON_CURRENT_MAJOR
   echo Latest nodejs: $LATEST
@@ -29,32 +23,45 @@ else
   NPM_VERSION=$(npm view npm version)
 fi
 
-echo Setting nodejs to $NODE_VERSION in .tool-versions ...
-#asdf set -p nodejs "${NODE_VERSION}"
-#asdf install nodejs
-#asdf current nodejs
-ASDF_VERSIONS_FILE=../.tool-versions
-cat ${ASDF_VERSIONS_FILE} | grep -v nodejs > ${ASDF_VERSIONS_FILE}.tmp
-echo nodejs $NODE_VERSION >> ${ASDF_VERSIONS_FILE}.tmp
-mv ${ASDF_VERSIONS_FILE}.tmp ${ASDF_VERSIONS_FILE}
+echo node and npm --version
+mise exec -- node --version
+mise exec -- npm --version
+echo
+echo Setting node to $NODE_VERSION in mise.toml ...
+mise use node@"${NODE_VERSION}"
+echo
+echo node and npm --version
+mise exec -- node --version
+mise exec -- npm --version
+echo
 
-#echo Updating npm to $NPM_VERSION ...
-#npm install -g npm@$NPM_VERSION
-
-PACKAGE_JSON_FILES=(package.json $(find packages -name package.json))
+PACKAGE_JSON_FILES=(package.json $(fd package.json packages/))
 echo Updating .engine in "${PACKAGE_JSON_FILES[@]}" ...
 for PACKAGE_JSON in  "${PACKAGE_JSON_FILES[@]}"; do
   echo Updating $PACKAGE_JSON
+  local tmp=$(mktemp)
+  echo tmp file: $tmp
   cat $PACKAGE_JSON | jq ".engines = {
   \"node\" : \"${NODE_VERSION}\",
   \"npm\" : \"${NPM_VERSION}\"
-  }" > temp.json \
-    && mv temp.json $PACKAGE_JSON
+  }" > "$tmp" && mv "$tmp" $PACKAGE_JSON
 done
 
-#echo Refresh engine in lock file
-#npm install --ignore-scripts --no-audit --package-lock-only --no-engine-strict
-#echo Sanity check with npm install
-#npm install
-#echo Run prettier check
-#npm run prettier-check-all
+# Chain commands in a single 'mise exec' process so the path shim works
+mise exec -- bash -c "
+  echo Refresh engine in lock file
+  mise exec -- npm install --ignore-scripts --no-audit --package-lock-only --no-engine-strict
+
+  echo Updating .packageManager in root package.json
+  corepack enable npm
+  corepack use npm@${NPM_VERSION}
+
+  echo Verifying active npm version:
+  npm --version
+
+  echo Sanity check with npm install
+  npm install
+
+  echo Run prettier check
+  npm run prettier-check-all
+"
